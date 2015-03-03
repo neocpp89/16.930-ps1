83c7cfbaa8cf5bba1cc763a12252e67bfd55f986
Sachith Dunatunga <neocpp@gmail.com>
2015-03-03 03:44:59 -0500

    Added error rates.

--- process_dg_errors.py
+++ process_dg_errors.py
@@ -18,24 +18,24 @@ def make_sure_path_exists(path):
     return
 
 # load errors from already-run simulations
-cg_errors = pickle.load(open('cg_error_pickle.p', 'rb'))
+dg_errors = pickle.load(open('dg_error_pickle.p', 'rb'))
 
 # reconstruct keys
-pdescs = cg_errors.keys()
-ETdescs = map(lambda x: x[1], sorted(map(lambda x: (-len(x.split(' ')), x), cg_errors[pdescs[0]].keys()), reverse=True))
-NumElements = sorted(cg_errors[pdescs[0]][ETdescs[0]].keys())
+pdescs = dg_errors.keys()
+ETdescs = map(lambda x: x[1], sorted(map(lambda x: (-len(x.split(' ')), x), dg_errors[pdescs[0]].keys()), reverse=True))
+NumElements = sorted(dg_errors[pdescs[0]][ETdescs[0]].keys())
 
 # output errors in a latex-friendly table using siunitx's '\num'
 for pd in pdescs:
     print r'\begin{table}[!h]'
     print r'\centering'
-    print r'\caption{'+pd+r': $L_2$ Error (CG methods)}'
+    print r'\caption{'+pd+r': $L_2$ Error (DG method)}'
     print r'\begin{tabular}{r | ' + ' '.join(['r']*len(ETdescs)) + r'}'
     print r'Number of Elements & ' + ' & '.join(ETdescs) + r' \\'
     print r'\midrule'
     for Ne in NumElements:
         print r'\num{{{:d}}} &'.format(Ne),
-        print ' & '.join(map(lambda ET: r'\num{{{:.4e}}}'.format(cg_errors[pd][ET][Ne][1]), ETdescs)) + r' \\'
+        print ' & '.join(map(lambda ET: r'\num{{{:.4e}}}'.format(dg_errors[pd][ET][Ne][1]), ETdescs)) + r' \\'
     print r'\end{tabular}'
     print r'\end{table}'
 
@@ -43,13 +43,13 @@ for pd in pdescs:
 for pd in pdescs:
     print r'\begin{table}[!h]'
     print r'\centering'
-    print r'\caption{'+pd+r': $H_1$ Error (CG methods)}'
+    print r'\caption{'+pd+r': $H_1$ Error (DG method)}'
     print r'\begin{tabular}{r | ' + ' '.join(['r']*len(ETdescs)) + r'}'
     print r'Number of Elements & ' + ' & '.join(ETdescs) + r' \\'
     print r'\midrule'
     for Ne in NumElements:
         print r'\num{{{:d}}} &'.format(Ne),
-        print ' & '.join(map(lambda ET: r'\num{{{:.4e}}}'.format(cg_errors[pd][ET][Ne][1]), ETdescs)) + r' \\'
+        print ' & '.join(map(lambda ET: r'\num{{{:.4e}}}'.format(dg_errors[pd][ET][Ne][1]), ETdescs)) + r' \\'
     print r'\end{tabular}'
     print r'\end{table}'
 
@@ -57,16 +57,16 @@ for pd in pdescs:
 for pd in pdescs:
     print r'\begin{table}[!h]'
     print r'\centering'
-    print r'\caption{'+pd+r': Convergence Rates (CG methods)}'
+    print r'\caption{'+pd+r': Convergence Rates (DG method)}'
     print r'\begin{tabular}{r | r r}'
     print r'Element & $L_2$ & $H_1$ \\'
     print r'\midrule'
     for ET in ETdescs:
-        upper = cg_errors[pd][ET][80][0]
-        lower = cg_errors[pd][ET][100][0]
+        upper = dg_errors[pd][ET][80][0]
+        lower = dg_errors[pd][ET][100][0]
         l2_rate = np.log10(upper/lower)/np.log10(100.0/80.0)
-        upper = cg_errors[pd][ET][80][1]
-        lower = cg_errors[pd][ET][100][1]
+        upper = dg_errors[pd][ET][80][1]
+        lower = dg_errors[pd][ET][100][1]
         h1_rate = np.log10(upper/lower)/np.log10(100.0/80.0)
         print ET+r' & ',
         print r'\num{{{:.3f}}} & '.format(l2_rate),
@@ -79,7 +79,7 @@ for pd in pdescs:
     f = pylab.figure(figsize=(3,3))
     for et in ETdescs:
         h = map(lambda x: 1.0/x, NumElements)
-        L2_errors = map(lambda x: cg_errors[pd][et][x][0], NumElements)
+        L2_errors = map(lambda x: dg_errors[pd][et][x][0], NumElements)
         pylab.loglog(h, L2_errors, styles.next())
     pylab.legend(ETdescs, loc='best', prop={'size': 8})
     pylab.title('$L_2$ error for ' + pd)
@@ -87,7 +87,7 @@ for pd in pdescs:
     pylab.xlabel('Element size $h$ [1/$N_e$]')
     pylab.tight_layout(pad=0.2)
     make_sure_path_exists('report/figs')
-    pylab.savefig('report/figs/l2_error_'+pd.split(' ')[0]+'.png')
+    pylab.savefig('report/figs/dg_l2_error_'+pd.split(' ')[0]+'.png')
     pylab.close(f)
         
 # generate plots for L2 error
@@ -95,7 +95,7 @@ for pd in pdescs:
     f = pylab.figure(figsize=(3,3))
     for et in ETdescs:
         h = map(lambda x: 1.0/x, NumElements)
-        H1_errors = map(lambda x: cg_errors[pd][et][x][1], NumElements)
+        H1_errors = map(lambda x: dg_errors[pd][et][x][1], NumElements)
         pylab.loglog(h, H1_errors, styles.next())
     pylab.legend(ETdescs, loc='best', prop={'size': 8})
     pylab.title('$H_1$ error for ' + pd)
@@ -103,6 +103,6 @@ for pd in pdescs:
     pylab.xlabel('Element size $h$ [1/$N_e$]')
     pylab.tight_layout(pad=0.2)
     make_sure_path_exists('report/figs')
-    pylab.savefig('report/figs/h1_error_'+pd.split(' ')[0]+'.png')
+    pylab.savefig('report/figs/dg_h1_error_'+pd.split(' ')[0]+'.png')
     pylab.close(f)
 
